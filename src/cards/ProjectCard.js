f78404659e88452ffbc3a13eed453925df59d0dc
Brian Uyeda <uyedacb@uw.edu>
2019-05-28 19:53:56 -0700

    stuff

---
+++ src/cards/ProjectCard.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './css/projectcard.css';
+import Card from 'react-bootstrap/Card';
+
+export default class ProjectCard extends Component {
+    render() {
+        return (
+            <Card className="bg-dark text-white">
+            <Card.Img src="holder.js/100px270" alt="Card image" />
+            <Card.ImgOverlay>
+                <Card.Title>Sports Link</Card.Title>
+                <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+            </Card>
+        )
+    }
+}
