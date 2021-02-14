4bb5410e8cccb125823660a6d47b0646bc07b72b
Uyedacb <uyedacb@uw.edu>
2021-02-13 18:57:35 -0800

    Updates

---
+++ precache-manifest.a2ba5c3cc9b8bbdd5084b4d870648292.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8d2d839de1d12c4cc6e67e80a78b75",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "729ea6429d05eabde314",
+    "url": "/portfolio/static/css/main.12b3e7c2.chunk.css"
+  },
+  {
+    "revision": "c1dd1e0611b67bf1f6b2",
+    "url": "/portfolio/static/js/2.02bfe8a0.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/portfolio/static/js/2.02bfe8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729ea6429d05eabde314",
+    "url": "/portfolio/static/js/main.dc9a6f76.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "273ea791f9d3f629e4e1613884278637",
+    "url": "/portfolio/static/media/banner.273ea791.jpeg"
+  },
+  {
+    "revision": "0f1f91316d07275184d01103556355c9",
+    "url": "/portfolio/static/media/linkedin.0f1f9131.svg"
+  },
+  {
+    "revision": "74b5ce0768d8940c6cff0aa785ad9ebd",
+    "url": "/portfolio/static/media/resume-uyeda.74b5ce07.pdf"
+  }
+]);
